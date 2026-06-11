ccda12ee637d96c8b8c58542123dd518a613d403
itc-s26006 <s26006@std.it-college.ac.jp>
2026-06-11 07:52:40 +0000

    第六章例題5まで

---
+++ Chapter06/ex6-02.py
@@ -0,0 +1,7 @@
+author = "kafka"
+print(author[0]) #インデックス番号は必ず0から
+print(author[1])
+print(author[2])
+print(author[3])
+print(author[4])
+print(author[5])
---
+++ Chapter06/ex6-04.py
@@ -0,0 +1,4 @@
+author = "kafka"
+print(author[-1]) #インデックス番号は必ず0から
+print(author[-2])
+print(author[-3])
