5fabcb3342b7c46fbd8ec46d10958670072472ea
itc-s26006 <s26006@std.it-college.ac.jp>
2026-06-25 07:42:50 +0000

    第七章チャレンジ問題終了

---
+++ chapter07/challenge7-04.py
@@ -0,0 +1,17 @@
+li=[1,2,3,4,5]
+
+while True:
+    a = input("入力してください:'qで終了'")
+    if a=="q":
+        break
+    try:
+        num=int(a)
+
+        if num in li:
+            print("正解")
+        else:
+            print("不正解")
+
+    except ValueError:
+        print("数字か'q'を入力してください")
+ 
---
+++ chapter07/challenge7-05.py
@@ -0,0 +1,8 @@
+[200~o=[8,19,148,4]
+  p=[9,1,33,83]
+
+  ty=[]
+  for i in o:
+      for u in p:
+           ty.append(i*u)
+  print(ty) 
