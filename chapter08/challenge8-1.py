a41157e4a085491083131cce2745ead686418c1e
itc-s26006 <s26006@std.it-college.ac.jp>
2026-06-30 07:52:44 +0000

    じゃんけんのやつ

---
+++ chapter08/challenge8-1.py
@@ -0,0 +1,27 @@
+import ranfom
+グー..1 チョキ..2 パー..3
+computer=random.randint(1,3)
+you=int(input("じゃんけんぽんん！(グー:1、チョキ:2、パー:3"))
+if you == 1: #グー
+    if computer==1:
+        print("あいこです")
+    if computer==2:
+        print("負けです")
+    if computur ==3:
+        print("勝ちです")
+elif you ==2: #チョキ
+    if computer==1:
+        print("勝ちです")
+    if computer==2:
+        print("あいこです")
+    if computer==3("負けです")
+elif you == 3: #パー
+    if computer ==1:
+        print(勝ち)
+    if computer == 2:
+        print(負けです)
+    if computer==3:
+        print(あいこです)
+
+
+
